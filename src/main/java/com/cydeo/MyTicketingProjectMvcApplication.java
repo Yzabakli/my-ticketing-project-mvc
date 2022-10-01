1e422816820f2f4ac3329e74a233083d0907872f
Yzabakli <abakliyusufziya@gmail.com>
2022-10-01 15:21:06 +0200

    Initial commit

---
+++ src/main/java/com/cydeo/MyTicketingProjectMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTicketingProjectMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTicketingProjectMvcApplication.class, args);
+    }
+
+}
